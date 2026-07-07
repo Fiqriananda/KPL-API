ec33ba5adc15e03e926206ed0e141182dff1f9bc
FairuzHamzahF4izal <fairuzhamzah1@gmail.com>
2026-07-07 22:12:09 +0800

    Controllers dsb

--- TUBESPTCAHATAREMBULANSEJATI/Controllers/DTOs/Auth/AuthResponseDto.cs
+++
@@ -1,9 +0,0 @@
-namespace ExpedisiPaketAPI.DTOs.Auth
-{
-    public class AuthResponseDto
-    {
-        public required string Token { get; set; }
-        public required string Role { get; set; }
-        public DateTime Expiration { get; set; }
-    }
-}
